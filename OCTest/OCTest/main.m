e57ceec1133bf7cfe398ea05188ce382c68d94f4
wangzexin <wangzexin19990311@163.com>
2024-01-02 15:36:16 +0800

    OC commit

---
+++ OCTest/OCTest/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  OCTest
+//
+//  Created by Xinnn on 2024/1/2.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
